eeceb47e2258504ee1b58cfe104a7a968d6bf697
Mansahib <mansahibsinghgalshi@gmail.com>
2025-01-01 23:29:47 +0530

    first commit

---
+++ src/utils/siteMetaData.js
@@ -0,0 +1,22 @@
+// please update required information
+const siteMetadata = {
+    title: 'Next.js Blog and about With Tailwind CSS and Contentlayer',
+    author: 'Mansahib',
+    headerTitle: 'Next.js CreativeCanvas',
+    description: 'A blog created with Next.js, Tailwind.css and contentlayer.',
+    language: 'en-us',
+    theme: 'system', // system, dark or light
+    siteUrl: 'https://create-blog-with-nextjs.vercel.app', // your website URL
+    siteLogo: '/logo.png',
+    socialBanner: '/social-banner.png', // add social banner in the public folder
+    email: 'mansahibsinghgalshi@gmail.com', 
+    github: 'https://github.com/Mansahib',
+    twitter: 'https://twitter.com',
+    facebook: 'https://facebook.com',
+    youtube: 'https://youtube.com',
+    linkedin: 'https://www.linkedin.com/in/mansahib-singh-92064a20a/',
+    dribbble: '',
+    locale: 'en-US',
+  }
+  
+  module.exports = siteMetadata
